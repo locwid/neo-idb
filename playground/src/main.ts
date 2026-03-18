37c33d7cebb49681c62fc70933be36f8b9c98045
Maxim Gritsina <maxim.gritsina.working@gmail.com>
2026-03-18 11:25:13 +0300

    feat: set up playground environment with initial files and configurations

---
+++ playground/src/main.ts
@@ -0,0 +1,2 @@
+import './style.css'
+import { neoIDB } from 'neo-idb'
--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     },
   },
   build: {
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'neo-idb',
